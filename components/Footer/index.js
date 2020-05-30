ae75850ef39f74f6d5815d8a3cc3ed5593a7c92a
beta <beta@softwareseni.com>
2020-05-31 01:27:05 +0700

    add footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,76 @@
 import Styles from './Footer.scss';
 
-import { Block } from '../Base';
+import { Block, Typography } from '../Base';
+import Container from '../../components/Base/Container';
 
 const Footer = (props) => {
-  return <Block className={[Styles.footer]}>Footer</Block>;
+  return (
+    <Block className={[Styles.footer]}>
+      <Container>
+        <Block w100 flex justifyBetween>
+          <Block className={[Styles.listItem]}>
+            <img
+              alt='Logo'
+              src='/static/img/logo.png'
+              className={Styles.logoBottom}
+            />
+          </Block>
+
+          <Block className={[Styles.listItem]}>
+            <Typography bold color='#000000' size={15}>
+              Vectordash
+            </Typography>
+
+            <Typography style={{ marginTop: '25px', cursor: 'pointer' }}>
+              Home
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              Login
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              Check Availability
+            </Typography>
+          </Block>
+          <Block className={[Styles.listItem]}>
+            <Typography bold color='#000000' size={15}>
+              Hosting
+            </Typography>
+
+            <Typography style={{ marginTop: '25px', cursor: 'pointer' }}>
+              Hosting Sign Up
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              My Machines
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              Documentation
+            </Typography>
+          </Block>
+          <Block className={[Styles.listItem]}>
+            <Typography bold color='#000000' size={15}>
+              FAQ
+            </Typography>
+
+            <Typography style={{ marginTop: '25px', cursor: 'pointer' }}>
+              Privacy Policy
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              Terms of Service
+            </Typography>
+
+            <Typography style={{ marginTop: '15px', cursor: 'pointer' }}>
+              Support
+            </Typography>
+          </Block>
+        </Block>
+      </Container>
+    </Block>
+  );
 };
 
 export default Footer;
