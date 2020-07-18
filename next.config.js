0221fac17e19cfc176c3c7a607379e26198bc366
beta <beta@softwareseni.com>
2020-07-19 01:45:41 +0700

    optimize image

--- next.config.js
+++ next.config.js
@@ -1,26 +1,30 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]',
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: [
-        {
-          loader: 'url-loader',
+const withOptimizedImages = require('next-optimized-images');
+module.exports = withOptimizedImages(
+  /*config for optimize image */
+  withSass({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]',
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
 
-          options: {
-            limit: 100000,
-            outputPath: 'static/',
-            publicPath: '/_next/',
+            options: {
+              limit: 100000,
+              outputPath: 'static/',
+              publicPath: '/_next/',
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    return config;
-  },
-});
+      return config;
+    },
+  })
+);
